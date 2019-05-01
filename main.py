93d7aff88e103009d35e6e0820bd32132325c51d
Elliot Lintz <elliot.lintz@gmail.com>
2019-05-02 01:00:14 +0200

    Possibilité de pixelisation à un certain niveau

--- main.py
+++ main.py
@@ -1,32 +1,45 @@
 import numpy as np
 import cv2
 
-name = "fleur.jpg"
+name = "arbre.jpg"
+path = "img/"+name
+scale = 5 # La largeur & hauteur des nouveaux 'pixels'
 
-img = cv2.imread(name, cv2.IMREAD_COLOR)
+img = cv2.imread(path, cv2.IMREAD_COLOR)
 
 height, width, channels = img.shape
+
+newHeight = int(height/scale)
+newWidth = int(width/scale)
+
 print(width, height)
+print(newWidth, newHeight)
 
-rouge = []
-vert = []
-bleu = []
+def moyenne(tab):
+	return int(round(sum(tab)/len(tab)))
 
-for x in range(width):
-	for y in range(height):
-		rouge.append(img[y,x][0])
-		vert.append(img[y,x][1])
-		bleu.append(img[y,x][2])
-
-moyR = round(sum(rouge)/len(rouge))
-moyV = round(sum(vert)/len(vert))
-moyB = round(sum(bleu)/len(bleu))
-print(moyR, moyV, moyB)
-
-img[0:height, 0:width] = [moyR, moyV, moyB]
+for x in range(newWidth):
+	x = x * scale
+	for y in range(newHeight):
+		y = y * scale
+		spotR = []
+		spotV = []
+		spotB = []
+		for spotX in range(scale):
+			for spotY in range(scale):
+				spotR.append(img[y+spotY,x+spotX][0])
+				spotV.append(img[y+spotY,x+spotX][1])
+				spotB.append(img[y+spotY,x+spotX][2])
+		spotR = moyenne(spotR)
+		spotV = moyenne(spotV)
+		spotB = moyenne(spotB)
+		img[y:y+scale, x:x+scale] = [spotR, spotV, spotB]
 
 cv2.imwrite('modif_'+name, img)
-
-cv2.imshow('image',img)
+cv2.imshow('modif_'+name,img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
+# Ajout
+# Possibilité de pixelisé les pixels non affecté (bords droits & bas)
+# quand la taille de l'image n'est pas un multiple du scale
